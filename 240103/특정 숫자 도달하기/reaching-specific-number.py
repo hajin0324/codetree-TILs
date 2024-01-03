309eb4c4774b59ee1759f4cfcec982fceddfa5af
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-03 16:28:56 +0900

    Title: 특정 숫자 도달하기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240103/특정 숫자 도달하기/reaching-specific-number.py
+++ 240103/특정 숫자 도달하기/reaching-specific-number.py
@@ -3,9 +3,9 @@ sum_val = 0
 cnt = 0
 
 for elem in arr:
-    if elem < 250:
-        sum_val += elem
-        cnt += 1
-    else:
-        print(f"{sum_val} {sum_val / cnt:.1f}")
+    if elem >= 250:
         break
+    sum_val += elem
+    cnt += 1
+
+print(f"{sum_val} {sum_val / cnt:.1f}")
