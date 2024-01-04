8e08ebe542c9f1a136176171eda166632a9d1d40
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-04 10:58:11 +0900

    Title: 500 근처의 수, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240104/500 근처의 수/near-500.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+max_val = 0
+min_val = 1001
+
+for elem in arr:
+    if elem < 500 and elem > max_val:
+        max_val = elem
+    
+    if elem > 500 and elem < min_val:
+        min_val = elem
+
+print(max_val, min_val)
