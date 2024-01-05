35d84cf074aa30a91dba088fc1c25bdcb596f52e
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-05 14:54:07 +0900

    Title: 첫 번째와 두 번째 교환, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
+++ 240105/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -4,8 +4,8 @@ sec = s[1]
 
 for i in range(len(s)):
     if s[i] == fir:
-        s[i] = s[:i] + sec + s[i + 1:]
+        s = s[:i] + sec + s[i + 1:]
     elif s[i] == sec:
-        s[i] = s[:i] + fir + s[i + 1:]
+        s = s[:i] + fir + s[i + 1:]
 
-print(''.join(arr))
+print(s)
