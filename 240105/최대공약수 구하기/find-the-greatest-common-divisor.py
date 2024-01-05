c8bd7b85e14d591bff5488ffe070e544eba41145
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-05 23:10:16 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240105/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,11 @@
+def find_gcd(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+
+    print(gcd)
+
+
+n, m = map(int, input().split())
+gcd(n, m)
