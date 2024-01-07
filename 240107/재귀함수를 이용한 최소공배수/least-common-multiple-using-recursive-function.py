96158037ee99cd57a114efe4bad6e9db7918b483
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-07 18:15:35 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240107/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240107/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 
-def calc(a, b):
+def lcm(a, b):
     for i in range(min(a, b), 0, -1):
         if a % i == 0 and b % i == 0:
             return (a * b) // i
@@ -14,7 +14,7 @@ def get_lcm(n):
     if n == 0:
         return arr[0]
 
-    return calc(get_lcm(n - 1), arr[n])
+    return lcm(get_lcm(n - 1), arr[n])
 
 
 print(get_lcm(n - 1))
