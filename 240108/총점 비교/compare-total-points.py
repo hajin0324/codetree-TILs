9928b1d7ccdbe69b7d9e11f66bad9968cfe17221
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-08 13:51:29 +0900

    Title: 총점 비교, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240108/총점 비교/compare-total-points.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, kor, eng, math = input().split()
+    students.append(Student(name, int(kor), int(eng), int(math)))
+
+students.sort(key=lambda x: x.kor + x.eng + x.math)
+
+for s in students:
+    print(s.name, s.kor, s.eng, s.math)
