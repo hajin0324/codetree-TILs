1b4b5835a2af0f4611260aed1c22bbb8b6101f74
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-08 14:54:40 +0900

    Title: 줄 세우기 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240108/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(self, height, weight, number):
+        self.height = height
+        self.weight = weight
+        self.number = number
+
+
+n = int(input())
+students = []
+for i in range(n):
+    height, weight = map(int, input().split())
+    students.append(Student(height, weight, i + 1))
+
+students.sort(key=lambda x: (x.height, -x.height))
+
+for s in students:
+    print(s.height, s.weight, s.number)
