cb37d28bcbc07b986406c31d63fa11d26a32aabb
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-10 01:29:40 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240110/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+
+a = [0] * 20000
+b = [0] * 20000
+
+idx = 1
+for i in range(n):
+    v, t = map(int, input().split())
+
+    for _ in range(t):
+        a[idx] = a[idx - 1] + v
+        idx += 1
+
+idx = 1
+for i in range(m):
+    v, t = map(int, input().split())
+
+    for _ in range(t):
+        b[idx] = b[idx - 1] + v
+        idx += 1
+
+cnt = -1
+f = 'o'
+for i in range(1, 20000):
+
+    if f != 'b' and a[i] < b[i]:
+        cnt += 1
+        f = 'b'
+    elif f != 'r' and a[i] > b[i]:
+        cnt += 1
+        f = 'r'
+
+print(cnt)
