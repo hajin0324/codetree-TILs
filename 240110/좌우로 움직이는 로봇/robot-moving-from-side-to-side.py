2c27123cad932a62f3383b787a9c86cc4606cf8a
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-10 11:29:44 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240110/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240110/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -6,32 +6,28 @@ b = [0] * 100000
 a_idx = 1
 for _ in range(n):
     t, d = input().split()
-    for i in range(int(t)):
+    for _ in range(int(t)):
             a[a_idx] = a[a_idx - 1] + (1 if d == 'R' else -1)
             a_idx += 1
 
 b_idx = 1
-for _ in range(n):
+for _ in range(m):
     t, d = input().split()
-    for i in range(int(t)):
+    for _ in range(int(t)):
             b[b_idx] = b[b_idx - 1] + (1 if d == 'R' else -1)
             b_idx += 1
 
 if a_idx > b_idx:
-    for i in range(b_idx, a_idx - b_idx):
-        b[i] = b[b_idx - 1]
-    b_idx = a_idx
+    for i in range(b_idx, a_idx):
+        b[i] = b[i - 1]
 
 elif b_idx > a_idx:
-    for i in range(a_idx, b_idx - a_idx):
-        a[i] = a[a_idx - 1]
-    a_idx = b_idx
+    for i in range(a_idx, b_idx):
+        a[i] = a[i - 1]
 
-cnt = -1
-same = False
-for i in range(1, a_idx):
-    if not same and a[i] == b[i]:
+cnt = 0
+for i in range(1, max(a_idx, b_idx)):
+    if a[i] == b[i] and a[i - 1] != b[i - 1]:
         cnt += 1
-    else:
-        same = False
+
 print(cnt)
