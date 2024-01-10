5a21e3c072ef70362b737d2b3a0fca096dc92ed9
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-10 17:19:31 +0900

    Title: 문자에 따른 명령 2, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240110/문자에 따른 명령 2/text-based-commands2.py
+++ 240110/문자에 따른 명령 2/text-based-commands2.py
@@ -1,13 +1,13 @@
-dirs = input()
+commands = input()
 x, y = 0, 0
 
 dir_num = 3
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 
-for c in dirs:
-    if c == "L":
+for cmd in commands:
+    if cmd == "L":
         dir_num = (dir_num - 1 + 4) % 4
-    elif c == "R":
+    elif cmd == "R":
         dir_num = (dir_num + 1) % 4
     else:
         x, y = x + dx[dir_num], y + dy[dir_num]
