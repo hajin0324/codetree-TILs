1b7596d34274ad1dc3f2139bfeb23fa01b8e2f3e
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-10 18:58:50 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240110/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240110/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -14,7 +14,7 @@ def in_range(x, y):
 
 
 answer[x][y] = 1
-for i in range(2, n * n + 1):
+for i in range(2, n * m + 1):
     nx, ny = x + dxs[dir_num], y + dys[dir_num]
 
     if not in_range(nx, ny) or answer[nx][ny] != 0:
