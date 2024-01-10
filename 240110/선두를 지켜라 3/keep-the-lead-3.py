6330961b93db682fd42714013612926d3e50daed
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-10 15:52:32 +0900

    Title: 선두를 지켜라 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240110/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,35 @@
+n, m = map(int, input().split())
+
+a_speed = [tuple(map(int, input().split())) for _ in range(n)]
+b_speed = [tuple(map(int, input().split())) for _ in range(m)]
+
+a = [0] * 100000
+b = [0] * 100000
+
+# 시간에 따른 A의 위치
+a_idx = 1
+for v, t in a_speed:
+    for _ in range(t):
+        a[a_idx] = a[a_idx - 1] + v 
+        a_idx += 1
+
+# 시간에 따른 B의 위치
+b_idx = 1
+for v, t in b_speed:
+    for _ in range(t):
+        b[b_idx] = b[b_idx - 1] + v
+        b_idx += 1
+
+leader, cnt = 0, 0
+for i in range(1, a_idx):
+    if a[i] > b[i] and leader != 1:
+        leader = 1
+        cnt += 1
+    elif a[i] < b[i] and leader != 2:
+        leader = 2
+        cnt += 1
+    elif a[i] == b[i] and leader != 3:
+        leader = 3
+        cnt += 1
+
+print(cnt)
