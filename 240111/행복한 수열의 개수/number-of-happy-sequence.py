e2c3f23bde9a10efdb787c1f670269cdcce59d4c
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-11 22:32:53 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240111/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240111/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,18 +5,30 @@ grid = [
 ]
 
 
-def happy_num(x, y):
-    global m
-    for i in range(y + m):
-        if grid[x][y] != grid[x][y + 1]:
+def happy_num(a, b):
+    global n, m
+
+    for i in range(m - 1):
+        if grid[a][b + i] != grid[a][b + i + 1]:
             return False
     return True
 
 
 ans = 0
-for i in range(n - m + 1):
+for i in range(n):
     for j in range(n - m + 1):
-        if happy_num(i, j) or happy_num(j, i):
+        if m == 1:
+            ans += 2
+            break
+
+        for k in range(m - 1):
+            if grid[i][j + k] != grid[i][j + k + 1]:
+                break
             ans += 1
 
+        for k in range(m - 1):
+            if grid[j][i + k] != grid[j][i + k + 1]:
+                break
+            ans += 1
+            
 print(ans)
