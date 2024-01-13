46d30b385262132c586d020e1ca4fe5e887e0d39
hajin <74534952+hajin0324@users.noreply.github.com>
2024-01-13 21:32:45 +0900

    Title: 그래프 탐색, Time: 276ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240113/그래프 탐색/graph-traversal.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+cnt = 0
+
+def bfs(vertex):
+    global cnt
+    for curr_v in graph[vertex]:
+        if not visited[curr_v]:
+            cnt += 1
+            visited[curr_v] = True
+            bfs(curr_v)
+
+
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+    graph[x].append(y)
+    graph[y].append(x)
+
+visited[1] = True
+bfs(1)
+
+print(cnt)
