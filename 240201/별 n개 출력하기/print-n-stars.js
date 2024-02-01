fc3f3be1bceb713d525404635bd2745fee3032db
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-01 20:50:35 +0900

    Title: 별 n개 출력하기, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

--- 240201/별 n개 출력하기/print-n-stars.js
+++ 240201/별 n개 출력하기/print-n-stars.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
-for (let i = 0; i < n; i++) {
+let i = 1;
+while (i <= n) {
     console.log("*");
+    i += 1;
 }
