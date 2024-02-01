05a84604de825b9eba7078dbc2fe89a908de45a5
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-01 14:51:46 +0900

    Title: 세 정수의 최솟값, Time: 247ms, Memory: 14MB, Status: Passed - CodeTree

--- 240201/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 240201/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -5,9 +5,9 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 let c = Number(input[2]);
 
-if (a < b && a < c) {
+if (a <= b && a <= c) {
     console.log(a);
-} else if (b < a && b < c) {
+} else if (b <= a && b <= c) {
     console.log(b);
 } else {
     console.log(c);
