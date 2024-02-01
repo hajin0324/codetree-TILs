097d05642a87b98ac49e45f332d9de69faa330ed
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-01 17:36:25 +0900

    Title: 코로나 메뉴얼, Time: 226ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240201/코로나 메뉴얼/covid-manual.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let count = 0;
+for (let i = 0; i < 3; i++) {
+    let p = input[i].split(" ");
+    if (p[0] && Number(p[1]) >= 37) {
+        count += 1;
+    }
+}
+
+if (count >= 2) {
+    console.log("E");
+} else {
+    console.log("N");
+}
