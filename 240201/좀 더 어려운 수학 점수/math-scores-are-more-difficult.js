a6065b4eb9a34645cbe042271f3618821858b416
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-01 15:55:57 +0900

    Title: 좀 더 어려운 수학 점수, Time: 184ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240201/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let score = fs.readFileSync(0).toString().trim().split("\n");
+
+let a = score[0].split(" ");
+let b = score[1].split(" ");
+
+let aMath = Number(a[0]);
+let aEng = Number(a[0]);
+let bMath = Number(b[0]);
+let bEng = Number(b[1]);
+
+if (aMath > bMath) {
+    console.log("A");
+} else if(bMath > aMath) {
+    console.log("B");
+} else {
+    console.log(aEng > bEng ? "A" : "B");
+}
