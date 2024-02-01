38cb1c79f717fa8c9434fb22e74027af8fedf4ca
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-01 17:48:39 +0900

    Title: 중앙값 구하기, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240201/중앙값 구하기/find-the-median.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+if (a > b) {
+    if (c > a) {
+        console.log(a);
+    } else {
+        console.log(c > b ? c : b);
+    }
+} else {
+    if (c > b) {
+        console.log(c);
+    } else {
+        console.log(c > a ? c : a);
+    }
+}
