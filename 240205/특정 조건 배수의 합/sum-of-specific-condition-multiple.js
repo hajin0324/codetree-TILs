7569292c1b99a3511b031c1ae836f2fd324d6f56
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-05 17:04:06 +0900

    Title: 특정 조건 배수의 합, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

--- 240205/특정 조건 배수의 합/sum-of-specific-condition-multiple.js
+++ 240205/특정 조건 배수의 합/sum-of-specific-condition-multiple.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 let sumVal = 0;
 
+if (input[0] > input[1]) input = [input[1], input[0]];
 for (let i = input[0]; i <= input[1]; i++) {
     if (i % 5 == 0) sumVal += i;
 }
