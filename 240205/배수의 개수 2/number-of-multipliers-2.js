431a30993b0723cf4de4c90bb7555b3cf585afca
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-05 16:25:19 +0900

    Title: 배수의 개수 2, Time: 219ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240205/배수의 개수 2/number-of-multipliers-2.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+
+let cnt = 0;
+for (let e of input) {
+    if (e % 2 != 0) cnt += 1;
+}
+
+console.log(cnt);
