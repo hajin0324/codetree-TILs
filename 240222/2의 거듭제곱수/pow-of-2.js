542fc4708aaa1dde3bc7f203b5fbfefdb29ad0e9
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-22 11:21:16 +0900

    Title: 2의 거듭제곱수, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240222/2의 거듭제곱수/pow-of-2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 1, x = 1;
+
+while (true) {
+    if ((2 ** x) == n) {
+        console.log(cnt);
+        break;
+    } 
+    x *= 2;
+    cnt++;
+}
